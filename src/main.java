a72aef0e716ca5906a104a8ac6ffccace6b9a2b9
eisakeramati <eisa.keramati@gmail.com>
2019-01-03 11:49:21 +0330

    first commit

---
+++ src/main.java
@@ -0,0 +1,33 @@
+import Algorithms.Genetic;
+import Algorithms.HillClimbing.FirstChoiceHC;
+import Algorithms.HillClimbing.RandomRestartHC;
+import Algorithms.HillClimbing.SimpleHC;
+import Algorithms.HillClimbing.StochasticHC;
+import Algorithms.SimulatedAnnealing;
+import Problems.*;
+
+public class main {
+
+    public static void main (String[] args){
+
+        GraphColoringProblem gcp = new GraphColoringProblem();
+
+       // new SimulatedAnnealing(gcp);
+      //  new Genetic(gcp);
+      // new FirstChoiceHC(gcp);
+      //  new RandomRestartHC(gcp, 4);
+      //  new SimpleHC(gcp);
+      //  new StochasticHC(gcp);
+        Genetic g = new Genetic(gcp);
+//        for (int i = 0; i < g.pSize; i++) {
+//            Problem.State s = g.population.get(i);
+//            s = (GraphColoringProblem.State) s;
+//            for (int j = 0; j < 11; j++) {
+//                System.out.println(((GraphColoringProblem.State) s).colors.get(j));
+//            }
+//            System.out.println("---------------------------");
+//        }
+//        System.out.println(g.parents.size());
+//
+    }
+}
